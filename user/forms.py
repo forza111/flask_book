72a008306586e18f1b27686c0f7c9a5eb1f389bc
forza111 <nikitos.ionkin@yandex.ru>
2020-10-23 08:50:09 +0300

    1

---
+++ user/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import Form
+from wtforms import validators,StringField,PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import ValidationError
+from user.models import User
+
+class RegisterForm(Form):
+    first_name = StringField('First Name', [validators.Required()])
+    last_name = StringField('Last Name', [validators.Required()])
+    email = EmailField('Email adress')
+    username = StringField('Username', [
+        validators.Required(),
+        validators.length(min=4, max=25)
+    ])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message=' Password must match'),
+        validators.length(min=4,max=80)
+    ])
+    confirm = PasswordField('Repeat Password')
+
+    def validate_username(form, field):
+        if User.objects.filter(username=field.data).first():
+            raise ValidationError("Username already exists")
+
+    def validate_email(form, field):
+        if User.objects.filter(email=field.data).first():
+            raise ValidationError("email is already in use")
